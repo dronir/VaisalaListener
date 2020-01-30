0d55a9206e8916eb34fe10684d183587602365e3
Olli Wilkman <olli.wilkman@iki.fi>
2020-01-30 16:14:48 +0200

    Switch test server to use asyncio

--- server_test.py
+++ server_test.py
@@ -1,4 +1,4 @@
-import socket
+import asyncio
 import time
 import logging
 from datetime import datetime
@@ -7,45 +7,45 @@ from datetime import datetime
 LINE_TEMPLATE = """(S:MAWS;D:{};T:{};TAAVG1M:{};PA:{:.1f})"""
 
 
-def create_data():
+def create_data(n):
     now = datetime.utcnow()
     date_str = now.strftime("%y%m%d")
     time_str = now.strftime("%H%M%S")
-    temp = 20.0
-    pressure = 1013.0
+    temp = 15.0 + n * 0.1
+    pressure = 1010.0 + n * 0.1
     return LINE_TEMPLATE.format(date_str, time_str, temp, pressure)
 
 
+async def handle_echo(reader, writer):
 
-def serve():
-    log_format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
+    addr = writer.get_extra_info('peername')
+    print(f"Connection from {addr!r}")
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("localhost", 42222))
-    sock.listen(5)
-    logging.info("Listening...")
+    n = 0
     try:
         while True:
-            (clientsocket, address) = sock.accept()
-            logging.info("Connection from {}".format(address))
-            while True:
-                message1 = bytes(create_data(), "utf-8")
-                try:
-                    clientsocket.send(message1)
-                except BrokenPipeError:
-                    logging.error("Lost connection")
-                    break
-                except Exception as E:
-                    logging.error("Unknown error:\n{}".format(repr(E)))
-                time.sleep(1.0)
+            if writer.is_closing():
+                break
+            print(f"Sending to {addr!r}.")
+            n += 1
+            writer.write(bytes(create_data(n), "utf-8"))
+            await writer.drain()
+            await asyncio.sleep(1)
+    except ConnectionResetError:
+        pass
     except KeyboardInterrupt:
-        sock.close()
-    except Exception as E:
-        logging.error("Unknown error:\n{}".format(repr(E)))
-        sock.close()
-    finally:
-        sock.close()
+        pass
 
-if __name__=="__main__":
-    serve()
+    print(f"Connection to {addr!r} closed.")
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(handle_echo, '127.0.0.1', 42222)
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}. Press ctrl-c to quit.')
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
