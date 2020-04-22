473060e3c9e98624907b1c234eaba54b7169a943
Olli Wilkman <olli.wilkman@iki.fi>
2020-04-22 16:09:30 +0300

    Have format regex handle space padding in message

--- listener.py
+++ listener.py
@@ -270,7 +270,7 @@ def has_time(data):
 
 def format_match(data):
     """Check if data string matches expected regular expression."""
-    m = re.fullmatch(r"\(\w+:\w+(;\w+:[\w\.\-(\/+)]+)+\)", data)
+    m = re.fullmatch(r"\([ \w]+:[\w ]+(;[\w ]+:[\w\.\-(\/+) ]+)+\)", data)
     return not (m is None)
 
 
