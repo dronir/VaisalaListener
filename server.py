0b59076a0f13a0f624034bd1db0f4f324cba7ee8
Olli Wilkman <olli.wilkman@csc.fi>
2021-11-02 11:53:31 +0200

    Refactor server with type hints

--- server.py
+++ server.py
@@ -1,46 +1,6 @@
 import asyncio
 import logging
-
-
-class MetCastProtocol(asyncio.Protocol):
-    def __init__(self, container):
-        self.container = container
-        self.transport = None
-        self.task = None
-
-    def connection_made(self, transport):
-        """When a client connects, start a task that writes data to the transport
-        every time the container is updated.
-        """
-        peername = transport.get_extra_info("peername")
-        logging.info(f"Server: New connection from {peername}.")
-        self.transport = transport
-        loop = asyncio.get_event_loop()
-        self.task = loop.create_task(self.serve_data())
-
-    async def serve_data(self):
-        """The main task: in an infinite loop, wait for the DataContainer's value to
-        change, and when it does, send that data to the client.
-        """
-        # Reminder for future self about how this async condition works:
-        # - Entering the `async with` block acquires the condition's lock (waiting for it if necessary).
-        # - `condition.wait()` releases the lock, then blocks until the container value is updated.
-        # - When notified, the `wait()` call reacquires the lock and returns.
-        # - The new value is retrieved from the container with `container.get()`.
-        # - Exiting the `with` block releases the lock once again.
-        # - Then we send the data we just read and loop back.
-        while True:
-            async with self.container.condition:
-                await self.container.condition.wait()
-                data = self.container.get()
-            logging.debug(f"Server: sending data: {data}")
-            self.transport.write(bytes(data, "utf8"))
-
-    def connection_lost(self, exc):
-        """When a client disconnects, kill the uploader task and close the transport."""
-        logging.info("Server: Connection lost.")
-        self.task.cancel()
-        self.transport.close()
+from typing import Any, Optional
 
 
 class DataContainer:
@@ -54,10 +14,10 @@ class DataContainer:
         self.value = ""
         self.condition = asyncio.Condition()
 
-    def get(self):
+    def get(self) -> Any:
         return self.value
 
-    async def set(self, value):
+    async def set(self, value: Any) -> None:
         """Acquire condition lock, update value, notify all tasks that wait for update."""
         # Reminder for future self part 2:
         # - Entering the `async with` block acquires the condition's lock (waiting if necessary).
@@ -71,12 +31,59 @@ class DataContainer:
             self.condition.notify_all()
 
 
-async def start_server(global_config, container):
+class MetCastProtocol(asyncio.Protocol):
+    def __init__(self, container: DataContainer):
+        self.container = container
+        self.transport: Optional[Any] = None
+        self.task: Optional[Any] = None
+
+    def connection_made(self, transport) -> None:
+        """When a client connects, start a task that writes data to the transport
+        every time the container is updated.
+        """
+        peername = transport.get_extra_info("peername")
+        logging.info(f"Server: New connection from {peername}.")
+        self.transport = transport
+        loop = asyncio.get_event_loop()
+        self.task = loop.create_task(self.serve_data())
+
+    async def serve_data(self) -> None:
+        """The main task: in an infinite loop, wait for the DataContainer's value to
+        change, and when it does, send that data to the client.
+        """
+        # Reminder for future self about how this async condition works:
+        # - Entering the `async with` block acquires the condition's lock (waiting for it if necessary).
+        # - `condition.wait()` releases the lock, then blocks until the container value is updated.
+        # - When notified, the `wait()` call reacquires the lock and returns.
+        # - The new value is retrieved from the container with `container.get()`.
+        # - Exiting the `with` block releases the lock once again.
+        # - Then we send the data we just read and loop back.
+        assert self.transport is not None
+        while True:
+            async with self.container.condition:
+                await self.container.condition.wait()
+                data = self.container.get()
+            logging.debug(f"Server: sending data: {data}")
+            self.transport.write(bytes(data, "utf8"))
+
+    def connection_lost(self, exc) -> None:
+        """When a client disconnects, kill the uploader task and close the transport."""
+        # We can assert these because this should never get called
+        # before self.connection_made which sets these:
+        assert self.transport is not None
+        assert self.task is not None
+        logging.info("Server: Connection lost.")
+        self.task.cancel()
+        self.transport.close()
+
+
+async def start_server(global_config: dict, container: DataContainer):
     config = global_config["broadcast"]
     loop = asyncio.get_running_loop()
-    server = await loop.create_server(
-        lambda: MetCastProtocol(container), "", config["port"]
-    )
+    server = await loop.create_server(lambda: MetCastProtocol(container), "", config["port"])
+    if server.sockets is None:
+        logging.error("Server: no sockets found")
+        return
     addr = server.sockets[0].getsockname()
     logging.info(f"Server: Starting TCP serving on {addr}.")
     async with server:
